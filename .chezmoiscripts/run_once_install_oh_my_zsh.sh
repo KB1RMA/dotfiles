3849fcfb99866369cf88a95444a827e30b16b3b3
Christopher Snyder <kb1rma@arrl.net>
2025-06-17 09:21:43 -0400

    feat: installing OhMyZsh and plugins via homebrew

---
+++ .chezmoiscripts/run_once_install_oh_my_zsh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
