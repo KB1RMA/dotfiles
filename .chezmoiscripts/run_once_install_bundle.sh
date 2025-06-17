b4480bd9b93a8b2c17995eb11ad757646fbaff23
Christopher Snyder <kb1rma@arrl.net>
2025-06-17 09:06:44 -0400

    fix: check if Vundle already installed and update

--- .chezmoiscripts/run_once_install_bundle.sh
+++ .chezmoiscripts/run_once_install_bundle.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ -d "~/.vim/bundle/Vundle.vim" ]; then
+    git -C "~/.vim/bundle/Vundle.vim" pull
+else
+    git clone https://github.com/VundleVim/Vundle.vim.git "~/.vim/bundle/Vundle.vim"
+fi
