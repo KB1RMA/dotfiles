d8cf17fe6bb5539fb3c86aa44f460ab33727f1f7
Christopher Snyder <kb1rma@arrl.net>
2025-06-17 09:29:54 -0400

    fix: conditionally installing ohmyzsh

--- .chezmoiscripts/run_once_install_oh_my_zsh.sh
+++ .chezmoiscripts/run_once_install_oh_my_zsh.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+if [ -d "~/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  
+  # Install plugins
+  git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
