bf391f38a716aabb73d186fbbae88e3f4fbccfc6
Christopher Snyder <cellulararrest@gmail.com>
2026-07-08 09:46:37 -0400

    chore: remove vestigial Vundle install script
    This repo fully migrated to Neovim + lazy.nvim (dot_config/nvim/lua/config/lazy.lua,
    vim/vi aliased to nvim in dot_zshrc.tmpl). Nothing loads a .vimrc or reads
    ~/.vim/bundle anymore, so run_once_10_install_bundle.sh just cloned Vundle
    on every fresh machine for no reason. Removing it and the matching
    bundle/* .gitignore entries.

--- .chezmoiscripts/run_once_10_install_bundle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -d "$HOME/.vim/bundle/Vundle.vim" ]; then
-    git -C "$HOME/.vim/bundle/Vundle.vim" pull
-else
-    git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
-fi
